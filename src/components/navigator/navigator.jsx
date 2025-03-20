223b33f89c6ef92100d1013e094dfca49b5c21d6
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-03-21 00:59:57 +0200

    Committing navigator skeleton component

---
+++ src/components/navigator/navigator.jsx
@@ -0,0 +1,11 @@
+import "./navigator.css";
+import { useState } from 'react'
+
+function Navigator() {
+    return (
+      <>
+        <h1>Welcome Navigator</h1>
+      </>
+    );
+}
+export default Navigator;
