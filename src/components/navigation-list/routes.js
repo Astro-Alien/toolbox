6e187f9785531cf8177f318be353449e1d046b4c
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-03-30 01:17:05 +0200

    committing changes

--- src/components/navigation-list/routes.js
+++ src/components/navigation-list/routes.js
@@ -3,5 +3,10 @@ export const routes = {
         component: 'Home',
         name: 'home',
         path: 'home/home',
+    },
+    "virtual-list": {
+        component: 'VirtualList',
+        name: 'virtual-list',
+        path: 'virtual-list/virtual-list',
     }
 };
---
+++ src/components/virtual-list/virtual-list.jsx
@@ -0,0 +1,13 @@
+import './virtual-list.css';
+import { React } from 'react';
+
+function VirtualList() {
+    return (
+        <>
+            <h1>Virtual List</h1>
+            <p>Virtual List component</p>
+        </>
+    );
+}
+
+export default VirtualList;
