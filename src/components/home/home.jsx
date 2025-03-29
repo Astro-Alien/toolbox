fbcad5687d7d00b86ee508d77cd58bb44de78fbc
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-03-30 01:31:43 +0200

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,8 @@
 function Home() {
   return (
     <>
-        <h1 className="text-3xl h-[100%] w-[100%]">Welcome To My Toolbox</h1>
+        <h1 className="flex justify-center content-center flex-wrap
+                      text-3xl h-[100%] w-[100%]">Welcome To My Toolbox</h1>
     </>
   );
 }
