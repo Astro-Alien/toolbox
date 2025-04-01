44361c118cbda81fb7fcb88bf34ff77794acafc4
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2025-04-02 00:15:19 +0200

    update

--- src/components/navigation/router-controler/router-controler.jsx
+++ src/components/navigation/router-controler/router-controler.jsx
@@ -1,17 +1,24 @@
 import { routes } from '../routes.js';
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
+import HomeWelcome from '../../home-welcome/home-welcome.jsx';
+import TodoList from '../../todo-list/todo-list.jsx';
+import VirtualList from '../../virtual-list/virtual-list.jsx';
+
 
 function RouterControler() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                {
+                {/* {
                     Object.values(routes).map((route, index) => {
                         const Component = lazy(() => import(`${route.fixed_path}`));
                         return <Route key={index} path={route.path} element={<Component />} />;
                     })
-                }
+                } */}
+                <Route path="/" element={<HomeWelcome />} />
+                <Route path="/todo-list" element={<TodoList />} />
+                <Route path="/virtual-list" element={<VirtualList />} />
             </Routes>
         </Suspense>
     );
